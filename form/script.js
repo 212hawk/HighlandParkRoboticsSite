6d9b8283959ac9024de539f52b573243c5894baa
babmas122 <babmas122@gmail.com>
2020-01-14 04:23:57 +0000

    Updated header on index.html, worked on form section

--- form/script.js
+++ form/script.js
@@ -1,38 +1,59 @@
 var connection;
 var outerkey; //derived outer key
 var keypair;//outer keypair
-var auth;//one-time authentication code
-//var userID;
-function startsocket(words){
-	connection = new WebSocket('ws://18.216.231.197:17385');
-	connection.onopen = function () {
-		keypair = keygen();
-		};
-		connection.onmessage = function (e) {
-			var msg = e.data;
-			console.log("MSG: \n"+msg+"\n");
-			outerkey = derive(keypair[0],msg);
-			send(keypair[1]+':'+encrypt(words,outerkey));
-		};
-		connection.onclose = function(){
-		}
-	}
-	connection.onerror = function(error){
-		gebi('connectionstatus').innerHTML = "Connection Status: Unable To Connect To Server";
-	}
+var auth;//one-time authentication code, currently unused
+var words;//To be sent
+hideerror();
+function submit(){
+  if(gebi('message').value.length==0){
+    error1();
+  }else{
+    words=b64enc(gebi('message').value)+':'+b64enc(gebi('name').value)+':';
+    words+=b64enc(gebi('email').value)+':'+b64enc(richtime());
+    sendmessage();
+  }
 }
-function updateconnectionstat(){
-	try{
-		gebi('connectionstatus').innerHTML='Connection Status: '+['Attempting To Connect','Connected','Connecting','Disconnected'][connection.readyState];
-	}catch(err){
-		gebi('connectionstatus').innerHTML='Connection Status: Disconnected';
-	}
+function sendmessage(){
+	connection = new WebSocket('wss://18.216.231.197:17385');
 }
-function send(mode,msg){
+connection.onopen = function () {
+  keypair = keygen();
+};
+connection.onmessage = function (e) {
+  var msg = e.data;
+  console.log("MSG: \n"+msg+"\n");
+  outerkey = derive(keypair[0],msg);
+  send(b64dec(keypair[1]+':'+encrypt(words,outerkey)));
+};
+connection.onclose = function(){
+  console.log("")
+}
+connection.onerror = function(error){
+  console.log("Connection Status: Unable To Connect To Server");
+  error2();
+}
+function send(msg){
 	console.log("Sending:\n"+mode+':'+msg);
 	if(connection.readyState==1){connection.send(mode+':'+msg);}
-	else{console.log('Send Error: WebSocket Starting/Closing: Readystate='+connection.readyState);}
+	else{console.log('Send Error: WebSocket Starting/Closing: Readystate='+connection.readyState);error2();}
 }
 function time(){
 	return new Date().getTime();
+}
+function richtime(){
+  return (new Date().toString());
+}
+function gebi(id){
+	return document.getElementById(id);
+}
+function error1(){
+	gebi("error").style.display = 'block';
+  gebi("error").innerHTML = 'Unable To Submit Message: No Message Was Typed';
+}
+function error2(){
+	gebi("error").style.display = 'block';
+  gebi("error").innerHTML = 'Unable To Submit Message Due To Server Error';
+}
+function hideerror(){
+	gebi("error").style.display = 'none';
 }
